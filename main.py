bcacffe819894060ccaa40f90e22a9f7ce452dd0
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-06-04 08:50:24 +0300

    IMagemodel

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException,File,UploadFile
 from huggingface_hub import snapshot_download
 from sentence_transformers import SentenceTransformer
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from transformers import CLIPModel, CLIPProcessor
+from PIL import Image
+import torch,io
+from typing import List
 
 # snapshot_download(
 #     repo_id="sentence-transformers/all-MiniLM-L6-v2",
@@ -16,7 +20,8 @@ app  = FastAPI()
 
 # Allow requests from your frontend
 origins = [
-    "http://localhost:3000",  # your Next.js frontend
+        "https://shopcheap.vercel.app/",
+        "http://localhost:3000",
 ]
 
 app.add_middleware(
@@ -28,19 +33,34 @@ app.add_middleware(
 )
 
 
-model = SentenceTransformer("E:/Model")
 class ProductData(BaseModel):
     whatToEmbed: str
- 
-@app.post("/embed")
+@app.on_event('startup')
+def load_model():
+        global model,ImageModel,processor
+        model = SentenceTransformer("Model")
+        ImageModel = CLIPModel.from_pretrained(r"E:\ImageModel")
+        processor = CLIPProcessor.from_pretrained(r"E:\ImageModel")
+         
+@app.post("/embed/text")
 async def add_embeddings(data:ProductData):
         searchData = data.whatToEmbed
         embeddings = model.encode(searchData)
         return {"success":True,"status":200,"embeddings": embeddings.tolist()}
 
-@app.get('/search')
-async def search(query: str):
-        embeddings = model.encode(query)
-        return {"embeddings": embeddings}
+@app.post('/embed/image')
+async def embedImage(files: List[UploadFile] = File(...)):
+        embeddings_list = []
+        
+        for file in files:
+                contents = await file.read()
+                image = Image.open(io.BytesIO(contents))
+                inputs = processor(images=image, return_tensors="pt")
 
         
+        with torch.no_grad():
+                image_embeddings = ImageModel.get_image_features(**inputs)
+                image_embeddings = image_embeddings / image_embeddings.norm(p=2, dim=-1, keepdim=True)
+                embeddings_list.append(image_embeddings.squeeze().tolist())
+                print(embeddings_list)
+        return {"success":True,"status":200,"embeddings":  embeddings_list}
--- sample.py
+++ sample.py
@@ -1,20 +1,29 @@
 from huggingface_hub import snapshot_download
 from sentence_transformers import SentenceTransformer
 from transformers import CLIPModel, CLIPProcessor
+from PIL import Image
+import torch
+import requests
 
-snapshot_download(
-    repo_id="openai/clip-vit-base-patch32",
-    local_dir=r"E:\ImageModel",  # you can set any path
-    local_dir_use_symlinks=False  # makes actual copies instead of symlinks
-)
+# snapshot_download(
+#     repo_id="openai/clip-vit-base-patch32",
+#     local_dir=r"E:\ImageModel",  
+#     local_dir_use_symlinks=False  
+# )
 
-model = CLIPModel.from_pretrained("E:\ImageModel")
-processor = CLIPProcessor.from_pretrained("E:\ImageModel")
+model = CLIPModel.from_pretrained(r"E:\ImageModel")
+processor = CLIPProcessor.from_pretrained(r"E:\ImageModel")
 print("✅ Model and processor loaded successfully.")
-# model = SentenceTransformer("E:\ImageModel")
-# print("Embedding shape:", model.encode("Test").shape)
 
-# sentences = ["This is an example sentence", "Each sentence is converted"]
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open("008_LandCruiser250FE.jpg")
 
-# embeddings = model.encode(sentences)
-# print(embeddings)
+inputs = processor(images=image, return_tensors="pt")
+
+with torch.no_grad():
+    image_embeddings = model.get_image_features(**inputs)
+
+image_embeddings = image_embeddings / image_embeddings.norm(p=2, dim=-1, keepdim=True)
+
+print("Image embeddings shape:", image_embeddings.shape)
+print("Image embeddings :", image_embeddings)
